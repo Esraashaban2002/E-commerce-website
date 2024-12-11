2f3e9ee00a61970dd30b2c2ce2029cae8ac4b3b8
Esraa <esraashaban2231@gmail.com>
2024-12-11 23:16:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, Login, Register } from "./pages";
 
+// GitHub Pages works better with HashRouter because it uses the # in the URL to handle routing on the client side
+
 function App() {
   return (
     <div className="App">
